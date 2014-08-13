889a082bf4bc23c7d1ab858fff4af8aae5f61309
Gwen <gwendall.esnault@gmail.com>
2014-08-14 01:12:41 +0200

    ok

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
 	
-	$(document).on("click", ".clearWay", function() {
-		way.remove();
-	});
-	
 	way.watchAll(function(selector, value) {
 		console.log("Something changed.", {
 			selector: selector,
 			value: value
 		});
 	});
-
+	
 });
