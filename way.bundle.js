c7b0466394f76be0e3acfdc75a54b1c9b664801e
Gwen <gwendall.esnault@gmail.com>
2014-08-15 19:31:29 +0200

    change input capture event from 'keyup' to 'input'

--- way.bundle.js
+++ way.bundle.js
@@ -1604,24 +1604,6 @@ window.way = {};
 		
 	}
 	
-	var isPrintableKey = function(e) {
-
-	    var keycode = e.keyCode;
-		if (!keycode) return true;
-
-	    var valid = 
-        	(keycode == 8)					 || // delete
-	        (keycode > 47 && keycode < 58)   || // number keys
-	        keycode == 32 || keycode == 13   || // spacebar & return key(s) (if you want to allow carriage returns)
-	        (keycode > 64 && keycode < 91)   || // letter keys
-	        (keycode > 95 && keycode < 112)  || // numpad keys
-	        (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
-	        (keycode > 218 && keycode < 223);   // [\]' (in order)
-
-	    return valid;
-		
-	}
-	
 	///////////////////////////////////
 	// INITIATE AND WATCH DOM EVENTS //
 	///////////////////////////////////
@@ -1651,9 +1633,8 @@ window.way = {};
 	});
 
 	var timeoutInput = null;
-	$(document).on("keyup change", "form[" + tagPrefix + "-data] :input", function(e) {
-
-		if (!isPrintableKey(e)) return;
+	$(document).on("input change", "form[" + tagPrefix + "-data] :input", function(e) {
+		
 		if (timeoutInput) clearTimeout(timeoutInput);
 		timeoutInput = setTimeout(function() {
 			var element = $(e.target).parents("form");
@@ -1662,9 +1643,8 @@ window.way = {};
 
 	});
 
-	$(document).on("keyup change", ":input[" + tagPrefix + "-data]", function(e) {
-		
-		if (!isPrintableKey(e)) return;
+	$(document).on("input change", ":input[" + tagPrefix + "-data]", function(e) {
+
 		if (timeoutInput) clearTimeout(timeoutInput);
 		timeoutInput = setTimeout(function() {
 			var element = $(e.target);
